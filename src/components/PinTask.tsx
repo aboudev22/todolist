92515881e9c3882f4154e570b634990d4f65001b
aboudev22 <aboubakarjay2@gmail.com>
2025-06-28 16:45:08 +0100

    Implement pin section

--- src/components/PinTask.tsx
+++ src/components/PinTask.tsx
@@ -1,14 +1,31 @@
-import { Pin } from "lucide-react";
+import { motion } from "framer-motion";
+import { BrushCleaning, Pin } from "lucide-react";
+import useTasks from "../store/tasksStore";
 
 export default function PinTask() {
+  const { pin, tasks } = useTasks();
+  const pinned = tasks.filter((t) => {
+    pin.includes(t.id);
+  });
+
   return (
-    <div className="w-full flex flex-col h-24">
+    <motion.div
+      initial={false}
+      animate={{ height: pin.length > 0 ? "auto" : "80px" }}
+      className="w-full flex flex-col h-24"
+    >
       <div className="w-full flex gap-2 items-center justify-center bg-neutral-400 rounded-md cursor-pointer">
         <p className="text-center font-bold text-black">pin</p>
-        <div>
-          <Pin size={20} color="#dcfce7" strokeWidth={3} />
-        </div>
+        <Pin size={20} color="#dcfce7" strokeWidth={3} />
       </div>
-    </div>
+      {pin.length > 0 ? (
+        pinned.map((t) => <p>{t.description.slice(0, 8)}</p>)
+      ) : (
+        <div className="w-full h-full flex flex-col justify-center items-center">
+          <BrushCleaning size={20} strokeWidth={1} color="#737373" />
+          <p className="text-xs text-neutral-500">empty</p>
+        </div>
+      )}
+    </motion.div>
   );
 }
