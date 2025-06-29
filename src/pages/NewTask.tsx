2804ea6ab06cf4f1c53eb82f19e487677f40e029
aboudev22 <aboubakarjay2@gmail.com>
2025-06-29 18:28:10 +0100

    29 june

--- src/pages/NewTask.tsx
+++ src/pages/NewTask.tsx
@@ -1,5 +1,17 @@
 export default function NewTask() {
   return (
-    <div className="w-full h-full bg-amber-50 flex justify-center items-center"></div>
+    <div className="w-full h-full bg-amber-50 flex justify-center items-center">
+      <form
+        action=""
+        className="flex flex-col w-xs justify-center items-center rounded-md py-16 bg-black/20"
+      >
+        <div>
+          <input type="text" placeholder="description..." />
+        </div>
+        <div>
+          <input type="date" placeholder="description..." />
+        </div>
+      </form>
+    </div>
   );
 }
